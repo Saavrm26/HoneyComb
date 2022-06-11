a16992c2744b72e214da9a56ae8744cd42ccb574
saarthak <lci2021035@iiitl.ac.in>
2022-06-11 05:41:34 +0530

    username moderation added

--- LoginTemplate/website/auth.py
+++ LoginTemplate/website/auth.py
@@ -4,69 +4,10 @@ from sqlalchemy import true
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-
+import requests
+import json
 auth = Blueprint('auth', __name__)
 
-# @auth.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         data=request.form
-#         print(data)
-#         email = request.form.get('email')
-#         print(email)
-#         password = request.form.get('password1')
-#         print(password)
-#         user = User.query.filter_by(email=email).first()
-#         if user:
-#             if check_password_hash(user.password, password):
-#                 flash('Logged in Successfully', category= 'success')
-#                 login_user(user, remember=True)
-#                 return redirect(url_for('views.home'))
-#             else:
-#                 flash('Incorrect password, try again', category='error')
-#         else:
-#             flash('Email doesn\'t exist ', category='error')
-
-#     return render_template("login.html",user=current_user)
-
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('auth.login'))
-
-# @auth.route('/sign-up', methods=['GET', 'POST'])
-# def sign_up():
-#     if request.method == 'POST':
-#         print(request.form)
-#         email = request.form.get('email')
-#         print(email)
-#         first_name = request.form.get('firstName')
-#         print(first_name)
-#         password1 = request.form.get('password1')
-#         print(password1)
-#         password2 = request.form.get('password2')
-#         print(password2)
-#         user = User.query.filter_by(email=email).first()
-#         if user:
-#             flash('Email already exists', category='error')
-#         elif len(email)<4:
-#             flash('Email length must be > 3 charcters.', category='error')
-#         elif len(first_name)<2:
-#             flash('Name should be more than one character.', category='error')
-#         elif password1 != password2:
-#             flash('Passwords don \'t match', category='error')
-#         elif len(password1)<7:
-#             flash('Password must be of atleast 7 characters.', category='error')
-#         else:
-#             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
-#             db.session.add(new_user)
-#             db.session.commit()
-#             flash('Account created successfully!', category='success')
-#             login_user(new_user, remember=True)
-#             return redirect(url_for('views.home'))
-
-#     return render_template("sign_up.html",user= current_user)
 email=''
 @auth.route('/',methods=['GET', 'POST'])
 def login():
@@ -98,8 +39,22 @@ def login():
             first_name = request.form.get('firstName')
             password1 = request.form.get('password1')
             password2 = request.form.get('password2')
+            data = {
+                'text': username,
+                'mode': 'username',
+                'lang': 'en',
+                'api_user': '45211228',
+                'api_secret': 'UwQPeDmdHyZBgpaKLUqF'
+                }
+            r = requests.post('https://api.sightengine.com/1.0/text/check.json', data=data)
+            output = json.loads(r.text)
+            profanity_match=len(output['profanity']['matches'])
+            misleading_match=len(output['misleading']['matches'])
+            print(output)
             user = User.query.filter_by(username=username).first()
-            if user:
+            if (profanity_match>0) or (misleading_match>0):
+                flash("Sorry you can't have that username")
+            elif user:
                 flash('Email already exists', category='error')
             elif len(email)<4:
                 flash('Email length must be > 3 charcters.', category='error')
